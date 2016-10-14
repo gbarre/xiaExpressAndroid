12d42d111f807dab6231b840924633d5d1f68607
Guillaume Barré <guillaume.barre@ac-versailles.fr>
2016-10-14 17:44:06 +0200

    Update licence

--- app/src/main/java/fr/ac_versailles/dane/xiaexpress/GridViewAdapter.java
+++ app/src/main/java/fr/ac_versailles/dane/xiaexpress/GridViewAdapter.java
@@ -10,10 +10,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.ArrayList;
 
-
 /**
- * Created by guillaume on 29/09/2016.
+ *
+ *  GridViewAdapter.java
+ *  XiaExpress
+ *
+ *  Created by guillaume on 29/09/2016.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ *  @author : guillaume.barre@ac-versailles.fr
  */
+
 public class GridViewAdapter extends ArrayAdapter<PhotoThumbnail> {
 
     private Context context;
