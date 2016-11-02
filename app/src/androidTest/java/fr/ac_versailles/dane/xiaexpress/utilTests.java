1c45aca82168f8515540e79e5a01f6ff020120a1
Guillaume Barré <guillaume.barre@ac-versailles.fr>
2016-11-02 18:19:54 +0100

    simple unitary tests

---
+++ app/src/androidTest/java/fr/ac_versailles/dane/xiaexpress/utilTests.java
@@ -0,0 +1,70 @@
+package fr.ac_versailles.dane.xiaexpress;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertEquals;
+
+import static fr.ac_versailles.dane.xiaexpress.Util.*;
+
+/**
+ * utilTests.java
+ * XiaExpress
+ *
+ * Created by guillaume on 02/11/2016.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * @author : guillaume.barre@ac-versailles.fr
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class utilTests {
+
+    private xiaDetail singlePointDetail = new xiaDetail(1, 1);
+    private xiaDetail multiplePointsDetail = new xiaDetail(1, 1);
+    private Context ctx = InstrumentationRegistry.getTargetContext();
+
+    public utilTests() {
+        // Single point detail
+        singlePointDetail.createPoint(10, 10, 0, 0, ctx);
+
+        // Multiple points detail
+        multiplePointsDetail.createPoint(10, 10, 0, 0, ctx);
+        multiplePointsDetail.createPoint(200, 150, 0, 1, ctx);
+        multiplePointsDetail.createPoint(120, 150, 0, 2, ctx);
+    }
+
+    @Test
+    public void modTest() throws Exception {
+        int output = mod(5, 3);
+        int expectedOutput = 2;
+        assertEquals(output, expectedOutput);
+    }
+
+    @Test
+    public void testUtilPointInPolygon() throws Exception {
+        // point in
+        Boolean outputIn = pointInPolygon(multiplePointsDetail.points, 130, 140);
+        Boolean expectedOutputIn= true;
+        assertEquals(outputIn, expectedOutputIn);
+
+        // point out
+        Boolean outputOut = pointInPolygon(multiplePointsDetail.points, 0, 0);
+        Boolean expectedOutputOut = false;
+        assertEquals(outputOut, expectedOutputOut);
+    }
+}
---
+++ app/src/androidTest/java/fr/ac_versailles/dane/xiaexpress/xiaDetailTests.java
@@ -0,0 +1,82 @@
+package fr.ac_versailles.dane.xiaexpress;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.ImageView;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * xiaDetailTest.java
+ * XiaExpress
+ *
+ * Created by guillaume on 02/11/2016.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * @author : guillaume.barre@ac-versailles.fr
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class xiaDetailTests {
+
+    private xiaDetail singlePointDetail = new xiaDetail(1, 1);
+    private xiaDetail multiplePointsDetail = new xiaDetail(1, 1);
+    private Context ctx = InstrumentationRegistry.getTargetContext();
+
+    public xiaDetailTests() {
+        // Single point detail
+        singlePointDetail.createPoint(10, 10, 0, 0, ctx);
+
+        // Multiple points detail
+        multiplePointsDetail.createPoint(10, 10, 0, 0, ctx);
+        multiplePointsDetail.createPoint(200, 150, 0, 1, ctx);
+        multiplePointsDetail.createPoint(120, 150, 0, 2, ctx);
+
+    }
+
+    @Test
+    public void testXiaDetailCreatePath() throws Exception {
+        // Single point
+        String output = singlePointDetail.createPath(0, 0);
+
+        String expectedOutput = "0;0";
+        assertEquals(output, expectedOutput);
+
+        // Multiple points
+        String output3pts = multiplePointsDetail.createPath(0, 0);
+        String expectedOutput3pts = "10.0;10.0 200.0;150.0 120.0;150.0";
+        assertEquals(output3pts, expectedOutput3pts);
+    }
+
+    @Test
+    public void testXiaDetailCreatePoint() throws Exception {
+        ImageView output = singlePointDetail.points.get(0);
+
+        ImageView expectedOutput = new ImageView(ctx);
+        expectedOutput.setImageResource(R.drawable.corner);
+        expectedOutput.setX(10);
+        expectedOutput.setY(10);
+        expectedOutput.setTag(1);
+
+        assertEquals(output.getTag(), expectedOutput.getTag());
+        assertEquals(output.getX(), expectedOutput.getX());
+        assertEquals(output.getY(), expectedOutput.getY());
+        assertEquals(output.getBackground(), expectedOutput.getBackground());
+    }
+
+
+}
