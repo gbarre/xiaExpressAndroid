f8e5d00c116d7c798ae278e85d5a6ee25a7201e8
Guillaume Barré <guillaume.barre@ac-versailles.fr>
2016-11-04 11:11:23 +0100

    Remove titleBar

--- app/src/main/java/fr/ac_versailles/dane/xiaexpress/CreateDetailActivity.java
+++ app/src/main/java/fr/ac_versailles/dane/xiaexpress/CreateDetailActivity.java
@@ -12,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -97,6 +99,9 @@ public class CreateDetailActivity extends AppCompatActivity implements AdapterVi
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // full screen, no title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_create_detail);
 
         String rectangle = getResources().getString(R.string.rectangle);
--- app/src/main/java/fr/ac_versailles/dane/xiaexpress/MainActivity.java
+++ app/src/main/java/fr/ac_versailles/dane/xiaexpress/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
@@ -61,6 +63,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // full screen, no title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         String TAG = Thread.currentThread().getStackTrace()[2].getClassName()+"."+Thread.currentThread().getStackTrace()[2].getMethodName();
